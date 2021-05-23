338769a13fbe0573ad5bf32c999172410f957e53
aaravpapalkar <71773470+aaravpapalkar@users.noreply.github.com>
2021-05-23 14:15:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+left_wristY = 0;
+right_wristY = 0;
 song = "";
 
 function preload() {
-
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function setup() {
@@ -9,8 +12,28 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        left_wristY = results[0].pose.leftWrist.y
+        right_wristY = results[0].pose.rightWrist.y;
+    }
 }
